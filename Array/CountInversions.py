5d9987c4a80228a3bac4d3890228f842bc493510
AayushiGandhi <ay753882@dal.ca>
2022-05-06 21:40:28 -0300

    count inversions

--- Array/CountInversions.py
+++ Array/CountInversions.py
@@ -1,19 +1,59 @@
-def inversionCount(arr, n):
-    if n == 0:
-        return 0
-    count = 0
-    flag = True
-    while flag:
-        flag = False
-        previous = 0
-        for next in range(1, n):
-            if arr[previous] > arr[next]:
-                count += 1
-                arr[previous], arr[next] = arr[next], arr[previous]
-                flag = True
-            previous = next
-    return count
+def mergeSort(arr, n):
+    temp_arr = [0] * n
+    return _mergeSort(arr, temp_arr, 0, n - 1)
+
+
+def _mergeSort(arr, temp_arr, left, right):
+    inv_count = 0
+    if left < right:
+        mid = (left + right) // 2
+        inv_count += _mergeSort(arr, temp_arr,
+                                left, mid)
+
+        inv_count += _mergeSort(arr, temp_arr,
+                                mid + 1, right)
+
+        inv_count += merge(arr, temp_arr, left, mid, right)
+    return inv_count
+
+
+def merge(arr, temp_arr, left, mid, right):
+    i = left  # Starting index of left subarray
+    j = mid + 1  # Starting index of right subarray
+    k = left  # Starting index of to be sorted subarray
+    inv_count = 0
+
+    while i <= mid and j <= right:
+        if arr[i] <= arr[j]:
+            temp_arr[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            # Inversion will occur.
+            temp_arr[k] = arr[j]
+            inv_count += (mid - i + 1)
+            k += 1
+            j += 1
+
+    while i <= mid:
+        temp_arr[k] = arr[i]
+        k += 1
+        i += 1
+
+    while j <= right:
+        temp_arr[k] = arr[j]
+        k += 1
+        j += 1
+
+    for loop_var in range(left, right + 1):
+        arr[loop_var] = temp_arr[loop_var]
+
+    return inv_count
 
 
 if __name__ == "__main__":
-    print(inversionCount([1, 20, 6, 4, 5], 5))
+    arr = [468, 335, 1, 170, 225, 479, 359, 463, 465, 206, 146, 282, 328, 462, 492, 496, 443, 328, 437, 392, 105, 403,
+           154, 293, 383, 422, 217, 219, 396, 448, 227, 272, 39, 370, 413, 168, 300, 36, 395, 204, 312, 323]
+    n = len(arr)
+    result = mergeSort(arr, n)
+    print("Number of inversions are", result)
