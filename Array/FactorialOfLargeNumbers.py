48f5923996aca3c848d5440013bc525e5f1111d1
AayushiGandhi <ay753882@dal.ca>
2022-05-11 16:22:20 -0300

    Max product

---
+++ Array/FactorialOfLargeNumbers.py
@@ -0,0 +1,19 @@
+def factorial(N):
+
+    return 0
+
+
+if __name__ == "__main__":
+    print(factorial(10))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Array/MaxProduct.py
@@ -0,0 +1,23 @@
+def maxProduct(arr, n):
+    minVal = arr[0]
+    maxVal = arr[0]
+    maxProduct = arr[0]
+
+    for i in range(1, n, 1):
+        # When multiplied by -ve number, maxVal becomes minVal and minVal becomes maxVal.
+        if (arr[i] < 0):
+            temp = maxVal
+            maxVal = minVal
+            minVal = temp
+
+        # maxVal and minVal stores the product of subarray ending at arr[i].
+        maxVal = max(arr[i], maxVal * arr[i])
+        minVal = min(arr[i], minVal * arr[i])
+
+        maxProduct = max(maxProduct, maxVal)
+
+    return maxProduct
+
+
+if __name__ == "__main__":
+    print(maxProduct([-1, -3, -10, 0, 60], 5))
