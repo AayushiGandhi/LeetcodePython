f36410ac2d51f459e07895da985c6b769ed15d39
AayushiGandhi <ay753882@dal.ca>
2022-05-11 16:24:09 -0300

    updated Max product

--- Array/MaxProduct.py
+++ Array/MaxProduct.py
@@ -3,12 +3,10 @@ def maxProduct(arr, n):
     maxVal = arr[0]
     maxProduct = arr[0]
 
-    for i in range(1, n, 1):
+    for i in range(1, n):
         # When multiplied by -ve number, maxVal becomes minVal and minVal becomes maxVal.
         if (arr[i] < 0):
-            temp = maxVal
-            maxVal = minVal
-            minVal = temp
+            minVal, maxVal = maxVal, minVal
 
         # maxVal and minVal stores the product of subarray ending at arr[i].
         maxVal = max(arr[i], maxVal * arr[i])
