88e831f59bfd4a9d3d200310511e9c11643c0af9
AayushiGandhi <ay753882@dal.ca>
2022-05-12 11:27:07 -0300

    factorial of large numbers

--- Array/FactorialOfLargeNumbers.py
+++ Array/FactorialOfLargeNumbers.py
@@ -1,10 +1,16 @@
-def factorial(N):
+def multiply(N):
+    fact = 1
+    for i in range(1, N+1):
+        fact *= i
+    return fact
 
-    return 0
+
+def factorial(N):
+    return str(multiply(N))
 
 
 if __name__ == "__main__":
-    print(factorial(10))
+    print(factorial(5))
 
 
 
---
+++ Array/MaxProfitAtMostTwice.py
@@ -0,0 +1,6 @@
+def maxProfit(n, price):
+    return 0
+
+
+if __name__ == "__main__":
+    print(maxProfit(6, [10, 22, 5, 75, 65, 80]))
