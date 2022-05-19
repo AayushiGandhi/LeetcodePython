af1309c9b5cb721e59c87b0462af6bdd952a42e3
AayushiGandhi <ay753882@dal.ca>
2022-05-19 13:52:55 -0300

    median in sorted matrix

---
+++ Matrix/MedianInSortedMatrix.py
@@ -0,0 +1,37 @@
+def countSmallerThanEqualToMid(row, mid):
+    low = 0
+    high = len(row) - 1
+
+    while low <= high:
+        m = (low + high) // 2
+        if row[m] <= mid:
+            low = m + 1
+        else:
+            high = m - 1
+
+    return low
+
+
+def binaryMedian(m, rowLen, columnLen):
+    low = 0
+    high = 10**9
+
+    while low <= high:
+        mid = (low + high) // 2
+        count = 0
+
+        for i in range(rowLen):
+            count += countSmallerThanEqualToMid(m[i], mid)
+
+        if count <= (rowLen * columnLen) // 2:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return low
+
+
+if __name__ == "__main__":
+    r, d = 3, 3
+    m = [[1, 3, 5], [2, 6, 9], [3, 6, 9]]
+    print(binaryMedian(m, r, d))
